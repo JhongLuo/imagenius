0d57b280dce0ad5099ae051816da50c4c0d87983
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2023-02-03 01:41:33 -0500

    make sure start.sh works (#14)

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-source venv/bin/activate
+pid_5000=$(lsof -ti tcp:5000)
+pid_5001=$(lsof -ti tcp:5001)
+
+kill -9 $pid_5000
+kill -9 $pid_5001
+
+source a1-backend/venv/bin/activate
 
 python3 a1-backend/init_db.py
 
