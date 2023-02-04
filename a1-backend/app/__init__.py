1188c6822f3249c45dd213c6d8c3cbe619fa220b
JhongLuo <1999luozhonghao@gmail.com>
2023-02-04 14:17:36 -0500

    fix cors error

--- a1-backend/app/__init__.py
+++ a1-backend/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from .app_operations import init_app
 import logging
+from flask_cors import CORS
+
 
 webapp = Flask(__name__)
+CORS(webapp, resources={r"/*": {"origins": "*"}})
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
