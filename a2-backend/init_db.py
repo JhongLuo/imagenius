920992f175d19a18bd2eb0843cf443a7bf5208a1
JhongLuo <1999luozhonghao@gmail.com>
2023-03-03 01:10:09 -0500

    initialize a2-backend

---
+++ a2-backend/init_db.py
@@ -0,0 +1,39 @@
+from utils.DBConnector import DBConnector
+from utils.ReplacementPolicies import ReplacementPolicies
+import mysql.connector
+
+cnx = mysql.connector.connect(
+    pool_name = "group18a1_pool",
+    pool_size = 16,
+    user='root',
+    password='ece1779pass',
+    host='localhost',
+    database='group18a1',
+)
+cursor = cnx.cursor()
+create_images = '''CREATE TABLE IF NOT EXISTS images (
+                    keyword VARCHAR(255) PRIMARY KEY,
+                    path VARCHAR(255) NOT NULL
+                )'''
+create_statistics = '''CREATE TABLE IF NOT EXISTS statistics (
+                    name VARCHAR(255) PRIMARY KEY,
+                    value DECIMAL(65, 3) NOT NULL
+                )'''    
+
+cursor.execute(create_images)
+cursor.execute(create_statistics)
+cnx.commit()
+cursor.close()
+cnx.close()
+DBConnector.delete_keys()
+
+initial_data = [
+    ('max_size', 100 * 1024 * 1024),
+    ('replacement_policy', ReplacementPolicies.LRU.value),
+    ('requests_count', 0),
+    ('requests_hit_count', 0),
+    ('items_len', 0),
+    ('items_bytes', 0),
+]
+
+DBConnector.add_statistics(initial_data)
