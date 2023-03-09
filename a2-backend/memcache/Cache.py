0f7309febabe2d00689887072d7271430fa4134b
JhongLuo <1999luozhonghao@gmail.com>
2023-03-08 22:36:41 -0500

    refactor: rewrite memcache using SortDict

--- a2-backend/memcache/Cache.py
+++ a2-backend/memcache/Cache.py
@@ -2,130 +2,127 @@ import sys
 from utils.ReplacementPolicies import ReplacementPolicies
 import threading
 from utils.DBConnector import DBConnector
+from sortedcontainers import SortedDict
+from random import randint
+import time
 
 class Node:
-    def __init__(self, key, value):
+    def __init__(self, key, value, hash_hex, time) -> None:
         self.key = key
+        self.hash_hex = hash_hex
         self.value = value
-        self.prev = None
-        self.next = None
+        self.time = time
 
-class Cache(dict):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+class Cache():
+    def __init__(self) -> None:
+        self.bst = SortedDict()
+        self.dict = dict()
         self.max_size = 0
-        self.policy = ReplacementPolicies.RANDOM
-        # implement LRU with double linked list
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
         self.bytes = 0
-        self._connect_linked_node(self.head, self.tail)
-        self.configuration_lock = threading.Lock()
-        self.linked_list_lock = threading.Lock()
-        self.write_lock = threading.Lock()
+        self.policy = ReplacementPolicies.RANDOM
+        self.writeLock = threading.Lock()
         self.syncDB()
         
+    def get_time(self):
+        return time.time()
+    
+    # TODO 
     def syncDB(self):
         DBConnector.set_statistics([('items_len', len(self)), ('items_bytes', self.bytes)])
         self.set_max_size(int(DBConnector.select_statistics('max_size')))
         self.set_policy(ReplacementPolicies.int2policy(DBConnector.select_statistics('replacement_policy')))
-    
-    # double linked list operations
-    
-    def _connect_linked_node(self, node1, node2):
-        node1.next = node2
-        node2.prev = node1
-
-    def _remove_linked_node(self, node):
-        with self.linked_list_lock:
-            self._connect_linked_node(node.prev, node.next)
-
-    def _add(self, node):
-        with self.linked_list_lock:
-            prev = self.tail.prev
-            self._connect_linked_node(prev, node)
-            self._connect_linked_node(node, self.tail)
-    
-    def _pop_linked_node(self):
-        with self.linked_list_lock:
-            node = self.head.next
-            self._connect_linked_node(node.prev, node.next)
+     
+    def _pop(self):
+        if len(self.bst) > 0:
+            if self.policy == ReplacementPolicies.LRU:                
+                _, node = self.bst.popitem(index=0)
+            else:
+                # randomly pop an item using random index
+                random_index = randint(0, len(self.bst) - 1)
+                node = self.bst.popitem(index=random_index)[1]
+            self.dict.pop(node.key)
+            self.bytes -= sys.getsizeof(node.value)
             return node
-
-    def _reset_linked_list(self):
-        with self.linked_list_lock:
-            self._connect_linked_node(self.head, self.tail)
-
-    # utils
-    
-    def _cache_pop(self):
-        if self.policy == ReplacementPolicies.LRU:
-            node = self._pop_linked_node()
-            super().__delitem__(node.key)
         else:
-            _, node = super().popitem()
-        self.bytes -= sys.getsizeof(node.value)
-        return node
-        
-    # override dict operations
+            return None
             
     def __getitem__(self, key):
-        node = super().__getitem__(key)
-        if self.policy == ReplacementPolicies.LRU:
-            self._remove_linked_node(node)
-            self._add(node)
-        return node.value
-        
-    def __setitem__(self, key, value):
-        if super().__contains__(key):
-            self.__delitem__(key)
-        with self.write_lock:
-            node = Node(key, value)
+        with self.writeLock:
+            node = self.dict[key]
             if self.policy == ReplacementPolicies.LRU:
-                self._add(node)
+                self.bst.pop(node.time)
+                node.time = self.get_time()
+                self.bst[node.time] = node
+            return node.value
+        
+    def __setitem__(self, key, value, hash_hex):
+        if key in self.dict:
+            self.__delitem__(key)
+        with self.writeLock:
+            node = Node(key, value, hash_hex, time.time())
+            self.dict[node.key] = node
+            self.bst[node.time] = node
             self.bytes += sys.getsizeof(value)
-            rtn = super().__setitem__(key, node)
             while self.bytes > self.max_size:
-                self._cache_pop()
-            return rtn    
+                self._pop()
+            return node
     
     def __delitem__(self, key) -> None:
-        with self.write_lock:
-            node = super().__getitem__(key)
-            if self.policy == ReplacementPolicies.LRU:
-                self._remove_linked_node(node)
+        with self.writeLock:
+            node = self.dict[key]
+            self.dict.pop(key)
+            self.bst.pop(node.time)
             self.bytes -= sys.getsizeof(node.value)
-            return super().__delitem__(key)
+            return
+    
+    # [lower, upper)
+    def get_range(self, lower, upper):
+            node_list = []
+            for _, node in self.dict.items():
+                if node.hash_hex < lower or node.hash_hex >= upper:
+                    continue
+                node_list.append(node)
+            return node_list
+    
+    # [lower, upper)
+    def delete_range(self, lower, upper):
+        for _, node in self.dict.items():
+            if node.hash_hex < lower or node.hash_hex >= upper:
+                continue
+            self.__delitem__(node.key)
+
+    # [lower, upper)
+    def merge_range(self, node_list):
+        with self.writeLock:
+            for node in node_list:
+                if node.time <= self.bst.peekitem(index=0)[0]:
+                    continue
+                self.dict[node.key] = node
+                self.bst[node.time] = node
+                while self.bytes > self.max_size:
+                    self._pop()
     
     def clear(self) -> None:
-        with self.write_lock:
-            self._reset_linked_list()
+        with self.writeLock:
+            self.dict = dict()
+            self.bst = SortedDict()
             self.bytes = 0
-            return super().clear()
         
     def get_bytes(self):
         return self.bytes
     
     def set_policy(self, policy):
-        if policy == self.policy:
-            return
-        with self.write_lock:
+        with self.writeLock:
             if policy in ReplacementPolicies:
-                self._reset_linked_list()
-                if policy == ReplacementPolicies.LRU:
-                    for _, node in self.items():
-                        self._add(node)
                 self.policy = policy
             else:
                 raise Exception("Unknown replacement policy")
 
     def set_max_size(self, max_size):
-        if max_size == self.max_size:
-            return
-        with self.write_lock:
+        with self.writeLock:
             if max_size >= 0:
                 self.max_size = max_size
                 while self.bytes > self.max_size:
-                    self._cache_pop()
+                    self._pop()
             else:
-                raise Exception("Invalid max size")        
+                raise Exception("Invalid max size")
