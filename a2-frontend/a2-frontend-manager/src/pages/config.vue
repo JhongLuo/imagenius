182bf48fb9f70f3f0b842f0a8acf218d720094c0
Tianwei Zhang <zhang13music@outlook.com>
2023-03-11 02:12:19 -0500

    fix: change ratio validity criteria, blur configs when loading

--- a2-frontend/a2-frontend-manager/src/pages/config.vue
+++ a2-frontend/a2-frontend-manager/src/pages/config.vue
@@ -18,14 +18,15 @@ const cacheConfigs = reactive<CacheConfigOptions>({
 })
 
 const cacheKeys = ref([])
-const isDownloading = ref(false)
+const isDownloadingCacheConfigs = ref(false)
+const isDownloadingCacheKeys = ref(false)
 const isModalPutCacheConfigsShown = ref(false)
 
 const cacheOptionsValidity = computed(() => {
   return {
     cacheSize: cacheConfigs.cacheSize !== '' && cacheConfigs.cacheSize >= 0,
-    expRatio: cacheConfigs.expRatio !== '' && cacheConfigs.expRatio > 1,
-    shrinkRatio: cacheConfigs.shrinkRatio !== '' && cacheConfigs.shrinkRatio > 0 && cacheConfigs.shrinkRatio < 1,
+    expRatio: cacheConfigs.expRatio !== '' && cacheConfigs.expRatio >= 1,
+    shrinkRatio: cacheConfigs.shrinkRatio !== '' && cacheConfigs.shrinkRatio >= 0 && cacheConfigs.shrinkRatio <= 1,
   }
 })
 
@@ -43,6 +44,7 @@ const isFormValid = computed(() => {
 })
 
 const handleGetCacheConfigs = async (isReload = false) => {
+  isDownloadingCacheConfigs.value = true
   // fetch data
   try {
     const response = await api.getCacheConfigsNew()
@@ -56,6 +58,7 @@ const handleGetCacheConfigs = async (isReload = false) => {
     cacheConfigs.shrinkRatio = response.data.shrinkRatio
     cacheConfigs.maxMiss = response.data.maxMiss * 100
     cacheConfigs.minMiss = response.data.minMiss * 100
+    isDownloadingCacheConfigs.value = false
     if (!isReload) {
       blinkToast(
         TOAST_ID__GET_CACHE_CONFIGS__SUCCESS,
@@ -105,7 +108,7 @@ const handlePutCacheConfigs = async () => {
 }
 
 const handleGetCacheKeys = async (isReload = false) => {
-  isDownloading.value = true
+  isDownloadingCacheKeys.value = true
   // fetch data
   try {
     const response = await api.getCacheKeys()
@@ -113,7 +116,7 @@ const handleGetCacheKeys = async (isReload = false) => {
     // handle success
     cacheKeys.value = response.data.keys
     await utils.sleep(150)
-    isDownloading.value = false
+    isDownloadingCacheKeys.value = false
     if (!isReload) {
       blinkToast(
         TOAST_ID__GET_CACHE_KEYS__SUCCESS,
@@ -128,7 +131,6 @@ const handleGetCacheKeys = async (isReload = false) => {
       TOAST_ID__GET_CACHE_KEYS__ERROR,
       'error',
       errMsg as string)
-    isDownloading.value = false
   }
 }
 
@@ -168,296 +170,300 @@ onMounted(() => {
 
   <!-- Page Content -->
   <ThePageContent>
-    <!-- Input Group -->
-    <TheInputGroup
-      w-xs
-      px-8 py-6
-      my-bg-secondary
-      my-card my-shadow-light
+    <div
+      :class="{ 'blur-sm grayscale': isDownloadingCacheConfigs }"
     >
-      <!-- Mode: manual / auto -->
-      <TheLabeledInput
-        input-id="input-manual-auto-mode"
-        label-text="Mode"
-        mb-3
+      <!-- Input Group -->
+      <TheInputGroup
+        w-xs
+        px-8 py-6
+        my-bg-secondary
+        my-card my-shadow-light
       >
-        <select
-          id="input-manual-auto-mode"
-          v-model="cacheConfigs.mode"
-          w-full p-2.5
-          text-sm my-text-color-primary
-          my-input
+        <!-- Mode: manual / auto -->
+        <TheLabeledInput
+          input-id="input-manual-auto-mode"
+          label-text="Mode"
+          mb-3
         >
-          <option disabled>
-            Choose one of the following:
-          </option>
-          <option value="manual">
-            Manual
-          </option>
-          <option value="auto">
-            Auto
-          </option>
-        </select>
-      </TheLabeledInput>
+          <select
+            id="input-manual-auto-mode"
+            v-model="cacheConfigs.mode"
+            w-full p-2.5
+            text-sm my-text-color-primary
+            my-input
+          >
+            <option disabled>
+              Choose one of the following:
+            </option>
+            <option value="manual">
+              Manual
+            </option>
+            <option value="auto">
+              Auto
+            </option>
+          </select>
+        </TheLabeledInput>
 
-      <!-- Num Nodes: Manual Change -->
-      <TheLabeledInput
-        input-id="input-num-nodes"
-        label-text="# of Nodes"
-      >
-        <input
-          id="input-num-nodes"
-          v-model="cacheConfigs.numNodes"
-          type="number"
-          min="1"
-          max="8"
-          step="1"
-          class="w-61.8%"
-          flex-grow-1
-          my-border
-          my-input disabled:pointer-events-none
-          data-tooltip-target="tooltip-num-nodes"
-          placeholder="<empty>"
-          onkeydown="return false"
-          :disabled="cacheConfigs.mode !== 'manual'"
-        >
-
-        <TheTooltip
-          id="tooltip-num-nodes"
-          label="1 <= Value <= 8"
-        />
-
-        <TheInputHelperText
-          helper-text="Manual mode only."
-        />
-      </TheLabeledInput>
-
-      <!-- Max Cache Size -->
-      <TheLabeledInput
-        input-id="input-max-cache-size"
-        label-text="Max Cache Size"
-      >
-        <div
-          flex w-full
+        <!-- Num Nodes: Manual Change -->
+        <TheLabeledInput
+          input-id="input-num-nodes"
+          label-text="# of Nodes"
         >
           <input
-            id="input-max-cache-size"
-            v-model="cacheConfigs.cacheSize"
+            id="input-num-nodes"
+            v-model="cacheConfigs.numNodes"
             type="number"
-            min="0"
+            min="1"
+            max="8"
+            step="1"
+            class="w-61.8%"
             flex-grow-1
-            my-border rounded-r-none
-            my-input
-            data-tooltip-target="tooltip-cache-size"
+            my-border
+            my-input disabled:pointer-events-none
+            data-tooltip-target="tooltip-num-nodes"
             placeholder="<empty>"
+            onkeydown="return false"
+            :disabled="cacheConfigs.mode !== 'manual'"
           >
-          <span
-            inline-flex items-center px-3
-            my-border rounded-l-none border-l-0
-            my-input select-none
-          >
-            MB
-          </span>
-        </div>
 
-        <TheTooltip
-          id="tooltip-cache-size"
-          label="Value ≥ 0"
+          <TheTooltip
+            id="tooltip-num-nodes"
+            label="1 <= Value <= 8"
+          />
+
+          <TheInputHelperText
+            helper-text="Manual mode only."
+          />
+        </TheLabeledInput>
+
+        <!-- Max Cache Size -->
+        <TheLabeledInput
+          input-id="input-max-cache-size"
+          label-text="Max Cache Size"
+        >
+          <div
+            flex w-full
+          >
+            <input
+              id="input-max-cache-size"
+              v-model="cacheConfigs.cacheSize"
+              type="number"
+              min="0"
+              flex-grow-1
+              my-border rounded-r-none
+              my-input
+              data-tooltip-target="tooltip-cache-size"
+              placeholder="<empty>"
+            >
+            <span
+              inline-flex items-center px-3
+              my-border rounded-l-none border-l-0
+              my-input select-none
+            >
+              MB
+            </span>
+          </div>
+
+          <TheTooltip
+            id="tooltip-cache-size"
+            label="Value ≥ 0"
+          />
+        </TheLabeledInput>
+
+        <!-- Replacement Policy -->
+        <TheLabeledInput
+          input-id="input-replacement-policy"
+          label-text="Replacement Policy"
+          mb-3
+        >
+          <select
+            id="input-replacement-policy"
+            v-model="cacheConfigs.policy"
+            w-full p-2.5
+            text-sm my-text-color-primary
+            my-input
+          >
+            <option disabled>
+              Choose one of the following:
+            </option>
+            <option value="LRU">
+              Least Recently Used
+            </option>
+            <option value="random">
+              Random Replacement
+            </option>
+          </select>
+        </TheLabeledInput>
+
+        <!-- Expand / Shrink Ratio -->
+        <TheLabeledInput
+          input-id="input-ratio"
+          label-text="Expand/Shrink Ratio"
+        >
+          <!-- row -->
+          <div
+            flex justify-between w-full
+          >
+            <!-- col: expRatio -->
+            <div
+              class="w-45%"
+              flex flex-col items-center
+            >
+              <input
+                id="input-ratio-expand"
+                v-model="cacheConfigs.expRatio"
+                type="number"
+                w-full h-full
+                my-border
+                my-input text-center
+                placeholder="<empty>"
+                :disabled="cacheConfigs.mode !== 'auto'"
+              >
+
+              <TheInputHelperText
+                helper-text="Expand Ratio ≥ 1"
+              />
+            </div>
+            <!-- col: shrinkRatio -->
+            <div
+              class="w-45%"
+              flex flex-col items-center
+            >
+              <input
+                id="input-ratio-shrink"
+                v-model="cacheConfigs.shrinkRatio"
+                type="number"
+                w-full h-full
+                my-border
+                my-input text-center
+                placeholder="<empty>"
+                :disabled="cacheConfigs.mode !== 'auto'"
+              >
+
+              <TheInputHelperText
+                helper-text="0 ≤ Shrink Ratio ≤ 1"
+              />
+            </div>
+          </div>
+        </TheLabeledInput>
+
+        <!-- Max / Min Miss -->
+        <TheLabeledInput
+          input-id="input-miss-rate"
+          label-text="Max/Min Miss Rate"
+        >
+          <!-- row -->
+          <div
+            flex justify-between w-full
+          >
+            <!-- col: maxMiss -->
+            <div
+              class="w-45%"
+              flex flex-col items-center
+            >
+              <div
+                flex w-full
+              >
+                <input
+                  id="input-miss-rate-max"
+                  v-model="cacheConfigs.maxMiss"
+                  type="number"
+                  oninput="this.value = this.value > 100 ? 100 : Math.floor(this.value)"
+                  w-full h-full
+                  my-border rounded-r-none
+                  my-input text-center
+                  data-tooltip-target="tooltip-max-miss"
+                  placeholder="<empty>"
+                  :disabled="cacheConfigs.mode !== 'auto'"
+                >
+                <span
+                  inline-flex items-center px-3
+                  my-border rounded-l-none border-l-0
+                  my-input select-none
+                >
+                  %
+                </span>
+              </div>
+
+              <TheTooltip
+                id="tooltip-max-miss"
+                label="Interger Only"
+              />
+
+              <TheInputHelperText
+                helper-text="Max Miss Rate"
+              />
+            </div>
+            <!-- col: minMiss -->
+            <div
+              class="w-45%"
+              flex flex-col items-center
+            >
+              <div
+                flex w-full
+              >
+                <input
+                  id="input-miss-rate-min"
+                  v-model="cacheConfigs.minMiss"
+                  type="number"
+                  oninput="this.value = this.value > 100 ? 100 : Math.floor(this.value)"
+                  w-full h-full
+                  my-border rounded-r-none
+                  my-input text-center
+                  data-tooltip-target="tooltip-min-miss"
+                  placeholder="<empty>"
+                  :disabled="cacheConfigs.mode !== 'auto'"
+                >
+                <span
+                  inline-flex items-center px-3
+                  my-border rounded-l-none border-l-0
+                  my-input select-none
+                >
+                  %
+                </span>
+              </div>
+
+              <TheTooltip
+                id="tooltip-min-miss"
+                label="Integers Only"
+              />
+
+              <TheInputHelperText
+                helper-text="Min Miss Rate"
+              />
+            </div>
+          </div>
+        </TheLabeledInput>
+
+        <!-- Button: Submit New Cache Configs -->
+        <TheButton
+          text-sm
+          label="Update"
+          :disabled="!isFormValid"
+          @click="isModalPutCacheConfigsShown = true"
         />
-      </TheLabeledInput>
 
-      <!-- Replacement Policy -->
-      <TheLabeledInput
-        input-id="input-replacement-policy"
-        label-text="Replacement Policy"
-        mb-3
-      >
-        <select
-          id="input-replacement-policy"
-          v-model="cacheConfigs.policy"
-          w-full p-2.5
-          text-sm my-text-color-primary
-          my-input
-        >
-          <option disabled>
-            Choose one of the following:
-          </option>
-          <option value="LRU">
-            Least Recently Used
-          </option>
-          <option value="random">
-            Random Replacement
-          </option>
-        </select>
-      </TheLabeledInput>
-
-      <!-- Expand / Shrink Ratio -->
-      <TheLabeledInput
-        input-id="input-ratio"
-        label-text="Expand/Shrink Ratio"
-      >
-        <!-- row -->
-        <div
-          flex justify-between w-full
-        >
-          <!-- col: expRatio -->
-          <div
-            class="w-45%"
-            flex flex-col items-center
-          >
-            <input
-              id="input-ratio-expand"
-              v-model="cacheConfigs.expRatio"
-              type="number"
-              w-full h-full
-              my-border
-              my-input text-center
-              placeholder="<empty>"
-              :disabled="cacheConfigs.mode !== 'auto'"
-            >
-
-            <TheInputHelperText
-              helper-text="Expand Ratio > 1"
-            />
-          </div>
-          <!-- col: shrinkRatio -->
-          <div
-            class="w-45%"
-            flex flex-col items-center
-          >
-            <input
-              id="input-ratio-shrink"
-              v-model="cacheConfigs.shrinkRatio"
-              type="number"
-              w-full h-full
-              my-border
-              my-input text-center
-              placeholder="<empty>"
-              :disabled="cacheConfigs.mode !== 'auto'"
-            >
-
-            <TheInputHelperText
-              helper-text="0 < Shrink Ratio < 1"
-            />
-          </div>
-        </div>
-      </TheLabeledInput>
-
-      <!-- Max / Min Miss -->
-      <TheLabeledInput
-        input-id="input-miss-rate"
-        label-text="Max/Min Miss Rate"
-      >
-        <!-- row -->
-        <div
-          flex justify-between w-full
-        >
-          <!-- col: maxMiss -->
-          <div
-            class="w-45%"
-            flex flex-col items-center
-          >
-            <div
-              flex w-full
-            >
-              <input
-                id="input-miss-rate-max"
-                v-model="cacheConfigs.maxMiss"
-                type="number"
-                oninput="this.value = this.value > 100 ? 100 : Math.floor(this.value)"
-                w-full h-full
-                my-border rounded-r-none
-                my-input text-center
-                data-tooltip-target="tooltip-max-miss"
-                placeholder="<empty>"
-                :disabled="cacheConfigs.mode !== 'auto'"
-              >
-              <span
-                inline-flex items-center px-3
-                my-border rounded-l-none border-l-0
-                my-input select-none
-              >
-                %
-              </span>
-            </div>
-
-            <TheTooltip
-              id="tooltip-max-miss"
-              label="Interger Only"
-            />
-
-            <TheInputHelperText
-              helper-text="Max Miss Rate"
-            />
-          </div>
-          <!-- col: minMiss -->
-          <div
-            class="w-45%"
-            flex flex-col items-center
-          >
-            <div
-              flex w-full
-            >
-              <input
-                id="input-miss-rate-min"
-                v-model="cacheConfigs.minMiss"
-                type="number"
-                oninput="this.value = this.value > 100 ? 100 : Math.floor(this.value)"
-                w-full h-full
-                my-border rounded-r-none
-                my-input text-center
-                data-tooltip-target="tooltip-min-miss"
-                placeholder="<empty>"
-                :disabled="cacheConfigs.mode !== 'auto'"
-              >
-              <span
-                inline-flex items-center px-3
-                my-border rounded-l-none border-l-0
-                my-input select-none
-              >
-                %
-              </span>
-            </div>
-
-            <TheTooltip
-              id="tooltip-min-miss"
-              label="Integers Only"
-            />
-
-            <TheInputHelperText
-              helper-text="Min Miss Rate"
-            />
-          </div>
-        </div>
-      </TheLabeledInput>
-
-      <!-- Button: Submit New Cache Configs -->
-      <TheButton
-        text-sm
-        label="Update"
-        :disabled="!isFormValid"
-        @click="isModalPutCacheConfigsShown = true"
-      />
-
-      <!-- Modal: Put Cache Configs -->
-      <TheModal
-        v-model:is-shown="isModalPutCacheConfigsShown"
-        modal-type="submit"
-        :modal-id="MODAL_ID__PUT_CACHE_CONFIGS"
-        :modal-title="MODAL_TITLE__PUT_CACHE_CONFIGS"
-        :modal-description="MODAL_DESCRIPTION__PUT_CACHE_CONFIGS({
-          mode: cacheConfigs.mode,
-          numNodes: cacheConfigs.numNodes,
-          cacheSize: cacheConfigs.cacheSize,
-          policy: cacheConfigs.policy,
-          expRatio: cacheConfigs.expRatio,
-          shrinkRatio: cacheConfigs.shrinkRatio,
-          maxMiss: cacheConfigs.maxMiss,
-          minMiss: cacheConfigs.minMiss,
-        } as CacheConfigOptions)"
-        :action="handlePutCacheConfigs"
-      />
-    </TheInputGroup>
+        <!-- Modal: Put Cache Configs -->
+        <TheModal
+          v-model:is-shown="isModalPutCacheConfigsShown"
+          modal-type="submit"
+          :modal-id="MODAL_ID__PUT_CACHE_CONFIGS"
+          :modal-title="MODAL_TITLE__PUT_CACHE_CONFIGS"
+          :modal-description="MODAL_DESCRIPTION__PUT_CACHE_CONFIGS({
+            mode: cacheConfigs.mode,
+            numNodes: cacheConfigs.numNodes,
+            cacheSize: cacheConfigs.cacheSize,
+            policy: cacheConfigs.policy,
+            expRatio: cacheConfigs.expRatio,
+            shrinkRatio: cacheConfigs.shrinkRatio,
+            maxMiss: cacheConfigs.maxMiss,
+            minMiss: cacheConfigs.minMiss,
+          } as CacheConfigOptions)"
+          :action="handlePutCacheConfigs"
+        />
+      </TheInputGroup>
+    </div>
 
     <!-- HR -->
     <TheConfigHR />
@@ -472,7 +478,7 @@ onMounted(() => {
       :modal-id="MODAL_ID__CLEAR_CACHE"
       :modal-title="MODAL_TITLE__CLEAR_CACHE"
       :modal-description="MODAL_DESCRIPTION__CLEAR_CACHE"
-      :class="{ 'blur-sm grayscale': isDownloading }"
+      :class="{ 'blur-sm grayscale': isDownloadingCacheKeys }"
       transition-all duration-300
     />
   </ThePageContent>
